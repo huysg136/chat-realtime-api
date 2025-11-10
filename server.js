78a1a35f6785751ff5dc93ddcd320f52229135fe
huysg136 <thaigiahuy6912@gmail.com>
2025-11-10 16:21:20 +0700

    fix

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.post("/api/ask-gemini", async (req, res) => {
   
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
