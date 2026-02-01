de36f748bfd1eb645e3619cfb682000b0e058e53
huysg136 <thaigiahuy6912@gmail.com>
2026-02-01 20:29:29 +0700

    feat: /room-token

--- src/Stringee/StringeeRoutes.js
+++ src/Stringee/StringeeRoutes.js
@@ -7,6 +7,9 @@ router.get("/token", stringeeController.getClientToken);
 router.post("/rest-token", stringeeController.getRestToken);
 router.post("/create-room", stringeeController.createRoom);
 router.post("/generate-room-token", stringeeController.getRoomToken);
+router.post("/room-token", stringeeController.getRoomToken);
 router.get("/list-rooms", stringeeController.listRooms);
+
+
 
 export default router;
