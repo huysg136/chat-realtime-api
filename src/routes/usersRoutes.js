3e0dca454e06d67cdb7578dd49f5bf5fd8796cf2
huysg136 <thaigiahuy6912@gmail.com>
2026-04-27 19:52:38 +0700

    feat: implement user registration notification service via Resend email API

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -4,6 +4,5 @@ import { usersController } from "../controllers/usersController.js";
 const router = express.Router();
 
 router.post("/new-user-notify", usersController.notifyNewUser.bind(usersController));
-router.get("/search", usersController.searchUsers.bind(usersController));
 
 export default router;
