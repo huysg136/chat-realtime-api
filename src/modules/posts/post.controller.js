5f80a195c9a1fb54168671d215b3ae4b36536953
huysg136 <thaigiahuy6912@gmail.com>
2026-08-20 21:41:21 +0700

    update: format

--- src/modules/posts/post.controller.js
+++ src/modules/posts/post.controller.js
@@ -2,22 +2,42 @@ import { postService } from "./post.service.js";
 
 export const createPost = async (req, res) => {
   const { content, mediaUrl, kind, privacy, fileSize } = req.body;
-  const result = await postService.createPost(req.user.uid, { content, mediaUrl, kind, privacy, fileSize });
+  const result = await postService.createPost(req.user.uid, {
+    content,
+    mediaUrl,
+    kind,
+    privacy,
+    fileSize,
+  });
   res.status(201).json({ success: true, ...result });
 };
 
 export const updatePost = async (req, res) => {
   const { content, mediaUrl, kind, privacy, fileSize } = req.body;
   const result = await postService.updatePost(req.user.uid, req.params.postId, {
-    content, mediaUrl, kind, privacy, fileSize,
+    content,
+    mediaUrl,
+    kind,
+    privacy,
+    fileSize,
   });
   res.status(200).json({ success: true, ...result });
 };
 
 export const getFeed = async (req, res) => {
-  const { filterUserId, searchQuery, skipCache, lastCreatedAt, limit: limitQuery } = req.query;
+  const {
+    filterUserId,
+    searchQuery,
+    skipCache,
+    lastCreatedAt,
+    limit: limitQuery,
+  } = req.query;
   const result = await postService.getFeed(req.user.uid, {
-    filterUserId, searchQuery, skipCache, lastCreatedAt, limitQuery,
+    filterUserId,
+    searchQuery,
+    skipCache,
+    lastCreatedAt,
+    limitQuery,
   });
   res.status(200).json({ success: true, ...result });
 };
@@ -28,9 +48,15 @@ export const likePost = async (req, res) => {
 };
 
 export const commentPost = async (req, res) => {
-  const { parentId, replyToUid, replyToName, content, postAuthorUid } = req.body;
+  const { parentId, replyToUid, replyToName, content, postAuthorUid } =
+    req.body;
   const result = await postService.commentPost(req.user.uid, {
-    postId: req.params.postId, parentId, replyToUid, replyToName, content, postAuthorUid,
+    postId: req.params.postId,
+    parentId,
+    replyToUid,
+    replyToName,
+    content,
+    postAuthorUid,
   });
   res.status(201).json({ success: true, ...result });
 };
@@ -39,7 +65,10 @@ export const likeComment = async (req, res) => {
   const { postId, commentId } = req.params;
   const { displayName, photoURL } = req.body;
   const result = await postService.likeComment(req.user.uid, {
-    postId, commentId, displayName, photoURL,
+    postId,
+    commentId,
+    displayName,
+    photoURL,
   });
   res.status(200).json({ success: true, ...result });
 };
@@ -56,6 +85,10 @@ export const checkNewPosts = async (req, res) => {
 
 export const deleteComment = async (req, res) => {
   const { postId, commentId } = req.params;
-  const result = await postService.deleteComment(req.user.uid, postId, commentId);
+  const result = await postService.deleteComment(
+    req.user.uid,
+    postId,
+    commentId,
+  );
   res.status(200).json({ success: true, ...result });
 };
